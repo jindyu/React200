ac33e3c8d9401e91674b0fd533fe8ab50e9c6fa8
jindyu <ihris219@gmail.com>
2021-12-25 11:47:12 +0900

    폴더 및 구분 26번까지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css'
-import ArrowFunc from './R0013_ArrowFunction';
+import ForceUpdate from './2_Notice/R026_ForceUpdate';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ArrowFunc/>
+      <ForceUpdate />
     </div>
   );
 }
