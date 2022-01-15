05e8fe4a08610aa2ec463c8b7e1feaf45060ba80
jindyu <ihris219@gmail.com>
2022-01-15 22:26:03 +0900

    last

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,69 @@
 import React, {Component} from 'react'
 import { Route, Routes } from 'react-router-dom'
+import cookie from 'react-cookies'
 
 import '../css/new.css'
 import HeaderAdmin from './Header/HeaderAdmin'
 import Footer from './Footer/Footer.js'
 import LoginFrom from './LoginForm'
-import ReactDebounce from './R094_reactDebounce'
-import ReactThrottle from './R095_reactThrottle'
-import ReactProxy from './R109_reactProxy'
-import ApiGetJson from './R110_ApiGetJson'
-import ApiPostJson from './R111_ApiPostJson'
-import FloatingPopulationList from './Floating_population/R096_floatingPopulationList'
 import SoftwareList from './SoftwareToolsManage/R126_SoftwareList'
 import SoftwareView from './SoftwareToolsManage/R130_SoftwareView'
+import Register from './Register/R148_Register'
+import axios from 'axios'
 
 // function App() {
 class App extends Component{
+
+  componentDidMount(){
+    axios.post('/api/LoginForm?type=SessionConfirm', {
+      token1 : cookie.load('userid'),
+      token2 : cookie.load('username')
+    })
+    .then(response => {
+      this.state.userid = response.data.token1
+      let password = cookie.load('userpassword')
+      if(password != undefined){
+        axios.post('/api/LoginForm?type=SessionSignin', {
+          is_Email: this.state.userid,
+          is_Token : password
+        })
+        .then(response => {
+          if(response.data.json[0].useremail === undefined){
+            this.noPermission()
+          }
+        })
+        .catch(error => {
+          this.noPermission()
+        })
+      } else {
+        this.noPermission()
+      }
+    })
+    .catch(response => this.noPermission())
+  }
+
+  noPermission = (e) => {
+    if(window.location.hash != 'nocookie'){
+      this.remove_cookie()
+      window.location.href = '/login/#nocookie'
+    }
+  }
+
+  remove_cookie = (e) => {
+    cookie.remove('userid', {path: '/'})
+    cookie.remove('username', {path: '/'})
+    cookie.remove('userpassword', {path: '/'})
+  }
+
   render(){
     return (
       <div className='App'>
         <HeaderAdmin />
           <Route exact path='/' component={LoginFrom} />
+          <Route path='/login' component={LoginFrom} />
           <Route exact path='/SoftwareList' component={SoftwareList} />
           <Route path='/SoftwareView/:swtcode' component={SoftwareView} />
+          <Route exact path='/register' component={Register} />
         <Footer />
       </div>
     );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import axios from "axios";
+import cookie from 'react-cookies';
+import Swal from 'sweetalert2';
+import $ from 'jquery';
 
 class LoginForm extends Component {
+
+    submitClick = (e) => {
+        this.email_val = $('#email_val').val();
+        this.pwd_val = $('#pwd_val').val();
+        if(this.email_val === '' || this.pwd_val === ''){
+            this.sweetalert('이메일과 비밀번호를 확인해주세요.', '', 'info', '닫기')
+        }else{
+            axios.post('/api/LoginForm?type=signin', {
+                is_Email: this.email_val,
+                is_Password: this.pwd_val
+            })
+            .then( response => {
+                var userid = response.data.json[0].useremail
+                var username = response.data.json[0].username
+                var upw = response.data.json[0].userpassword
+                
+                if(userid != null && userid != ''){
+                    this.sweetalert('로그인 되었습니다.', '', 'info', '닫기')
+                    const expires = new Date()
+                    expires.setMinutes(expires.getMinutes() + 60)
+                    
+                    axios.post('/api/LoginForm?type=SessionState', {
+                        is_Email: userid,
+                        is_UserName: username,
+                    })
+                    .then( response => {
+                        cookie.save('userid', response.data.token1
+                        , { path: '/', expires })
+                        cookie.save('username', response.data.token2
+                        , { path: '/', expires })
+                        cookie.save('userpassword', upw
+                        , { path: '/', expires })
+                    })  
+                    .catch( error => {
+                        this.sweetalert('작업중 오류가 발생하였습니다.', error, 'error', '닫기');
+                    });
+                    
+                    setTimeout(function() {
+                        window.location.href = '/SoftwareList';
+                    }.bind(this),1000);
+                }else{
+                    this.sweetalert('이메일과 비밀번호를 확인해주세요.', '', 'info', '닫기')
+                }
+            })
+            .catch( error => {this.sweetalert('이메일과 비밀번호를 확인해주세요.', '', 'info', '닫기')} );
+        }
+    }
+
+    sweetalert = (title, contents, icon, confirmButtonText) => {
+        Swal.fire({
+            title: title,
+            text: contents,
+            icon: icon,
+            confirmButtonText: confirmButtonText
+          })
+    }
+
     render () {
         return (
             <section className="main">
@@ -18,7 +79,7 @@ class LoginForm extends Component {
                         <input type="password" id="pwd_val" name="password" placeholder="비밀번호" onChange={this.handleChange} />
                     </div>
                     <ul className="af">
-                        <li><Link to={'/register_check'}>회원가입</Link></li>
+                        <li><Link to={'/register'}>회원가입</Link></li>
                         <li className="pwr_b" onClick={this.pwdResetClick}><a href="#n">비밀번호 재설정</a></li>
                     </ul>
                     <button className="s_bt" type="submit" onClick={this.submitClick}>로그인</button>
